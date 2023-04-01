5a996d27a74a77563f0cb33a96b23c3d9b6ea338
Gerson Mendoza <gerbo0906@gmail.com>
2023-04-01 10:54:36 -0600

    CleanArchitecture: structure definition

---
+++ CleanArchitecture/CleanArchitecture.Infrastructure/StreamerDbContext.cs
@@ -0,0 +1,56 @@
+﻿using CleanArchitecture.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Data
+{
+    public class StreamerDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=gerbo-qa.database.windows.net,1433;
+                                          Initial Catalog=Streamer;
+                                          Persist Security Info=False;
+                                          User ID=user-develop;
+                                          Password=S1st3m@s*;
+                                          MultipleActiveResultSets=False;
+                                          Encrypt=True;
+                                          TrustServerCertificate=False;
+                                          Connection Timeout=30;")
+               .LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name },
+                   Microsoft.Extensions.Logging.LogLevel.Information)
+               .EnableSensitiveDataLogging();
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Streamer>()
+                .HasMany(m => m.Videos)
+                .WithOne(m => m.Streamer)
+                .HasForeignKey(m => m.StreamerId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Restrict);
+
+
+
+            modelBuilder.Entity<Video>()
+                .HasMany(p => p.Actores)
+                .WithMany(t => t.Videos)
+                .UsingEntity<VideoActor>(
+                    pt => pt.HasKey(e => new { e.ActorId, e.VideoId })
+                );
+
+        
+        }
+
+
+        public DbSet<Streamer>? Streamers { get; set; }
+
+        public DbSet<Video>? Videos { get; set; }
+
+        public DbSet<Actor>? Actores { get; set; }
+
+        public DbSet<Director>? Directores { get; set; }
+
+    }
+}
